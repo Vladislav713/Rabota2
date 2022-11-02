23572ada7f4d59702172542f65bd555f16013856
Vladislav <vladperov@mail.ru>
2022-11-02 15:11:23 +0500

    3

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[5];
+int temp = number;
+
+for (int i = 0; i<array.Length; i++)
+{
+    number = temp;
+    array[i] = number %= 10;
+    temp /= 10;
+}
+
+if (array[0] == array[4] && array[1] == array[3]) 
+{ 
+    Console.WriteLine("Является палиндромом"); 
+}
+else 
+{ 
+    Console.WriteLine("Не является палиндромом"); 
+}
