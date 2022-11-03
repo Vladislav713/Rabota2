6f64ee10051ed93141bddf2b26c45c407ca7a993
Vladislav <vladperov@mail.ru>
2022-11-03 17:01:13 +0500

    4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[10];
+int temp = a;
+int result=0;
+
+for (int i = 0; i<array.Length; i++)
+{
+    a = temp;
+    array[i] = a %= 10;
+    temp /= 10;
+    result=array[i]+result;
+}
+Console.WriteLine(result);
