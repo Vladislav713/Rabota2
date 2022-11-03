cfe54142101455e80a3f421c3605877b80be2a28
Vladislav <vladperov@mail.ru>
2022-11-03 16:46:06 +0500

    3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B) и 
+//возводит число A в целую степень B
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result =a;
+for (int i = 1 ; i<b ; i++)
+{
+    result=result*a;    
+}
+Console.WriteLine(result);
